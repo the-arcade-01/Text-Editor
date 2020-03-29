b03185a356bfbad0228d2dce74be75e9d8f2c98a
aashisharcade@gmail.com <aashisharcade@gmail.com>
2020-03-30 01:41:10 +0530

    added menubar

--- code.py
+++ code.py
@@ -4,8 +4,25 @@ from tkinter import *
 import tkinter.scrolledtext as scrolledtext
 
 window = Tk(className = ' Text Editor')
-text = scrolledtext.ScrolledText(window,width = 100, height=50)
+text = scrolledtext.ScrolledText(window,width = 100, height=80)
 text.pack()
+
+# creating a menu in the window
+menu  = Menu(window)
+window.config(menu = menu)
+
+filemenubar = Menu(menu)
+# adding a File button the menubar
+menu.add_cascade(label='File',menu = filemenubar)
+# adding options to the File option
+filemenubar.add_command(label = 'New')
+filemenubar.add_command(label = 'Open')
+filemenubar.add_command(label = 'Save')
+filemenubar.add_separator()
+filemenubar.add_command(label = 'Exit')
+
+menu.add_cascade(label = 'Help')
+menu.add_cascade(label = 'About')
 
 # keeping the window open
 window.mainloop()
